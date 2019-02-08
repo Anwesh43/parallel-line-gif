55a2a5ff762ad3547abb564f01fcbf6cd25a51de
Anwesh43 <anweshthecool0@gmail.com>
2019-02-08 20:15:03 +0530

    adding State class

--- ParallelLineGif.js
+++ ParallelLineGif.js
@@ -52,3 +52,28 @@ const drawPLNode = (context, i, scale) => {
     }
     context.restore()
 }
+
+class State {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(cb) {
+        this.scale += updateValue(this.scale, this.dir, lines, lines)
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
