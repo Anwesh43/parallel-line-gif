ba5b56c3fb12bd3f23448adf0530329550c1a95f
Anwesh43 <anweshthecool0@gmail.com>
2019-02-08 19:47:32 +0530

    adding constants

--- ParallelLineGif.js
+++ ParallelLineGif.js
@@ -1,3 +1,14 @@
+const w = 600
+const h = 600
+const scGap = 0.05
+const scDiv = 0.51
+const lineColor = "#0277BD"
+const backColor = "#212121"
+const Canvas = require('canvas').Canvas
+const GifEncoder = require('gifencoder')
+const sizeFactor : number = 3
+const strokeFactor : number = 90
+
 const maxScale = (scale, i, n) => Math.max(0, scale - i / n)
 const divideScale = (scale, i, n) => Math.min(1/n, scale - i / n) * n
 const scaleFactor = (scale) => Math.floor(scale / 0.51)
